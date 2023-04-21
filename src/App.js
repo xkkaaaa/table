5e97499b0106e4a6cd96ca280d949508df6b7be7
Beknazar <cholponbaevb655@gmail.com>
2023-04-21 20:40:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,395 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Table from './components/Table'
+
+const data = [
+  {
+    store: {
+      id: 1,
+      name: 'Store 1',
+    },
+    months: [
+      { id: '1_1', value: 0 },
+      { id: '1_2', value: 0 },
+      { id: '1_3', value: 0 },
+      { id: '1_4', value: 0 },
+      { id: '1_5', value: 0 },
+      { id: '1_6', value: 0 },
+      { id: '1_7', value: 0 },
+      { id: '1_8', value: 0 },
+      { id: '1_9', value: 0 },
+      { id: '1_10', value: 0 },
+      { id: '1_11', value: 0 },
+      { id: '1_12', value: 0 },
+    ],
+  },
+  {
+    store: {
+      id: 2,
+      name: 'Store 2',
+    },
+    months: [
+      { id: '2_1', value: 0 },
+      { id: '2_2', value: 0 },
+      { id: '2_3', value: 0 },
+      { id: '2_4', value: 0 },
+      { id: '2_5', value: 0 },
+      { id: '2_6', value: 0 },
+      { id: '2_7', value: 0 },
+      { id: '2_8', value: 0 },
+      { id: '2_9', value: 0 },
+      { id: '2_10', value: 0 },
+      { id: '2_11', value: 0 },
+      { id: '2_12', value: 0 },
+    ],
+  },
+  {
+    store: {
+      id: 5,
+      name: 'Store 5',
+    },
+    months: [
+      {
+        id: '1_5',
+        name: 'JAN',
+        value: 0,
+      },
+      {
+        id: '2_5',
+        name: 'FEB',
+        value: 0,
+      },
+      {
+        id: '3_5',
+        name: 'MAR',
+        value: 0,
+      },
+      {
+        id: '4_5',
+        name: 'APR',
+        value: 0,
+      },
+      {
+        id: '5_5',
+        name: 'MAY',
+        value: 0,
+      },
+      {
+        id: '6_5',
+        name: 'JUN',
+        value: 0,
+      },
+      {
+        id: '7_5',
+        name: 'JUL',
+        value: 0,
+      },
+      {
+        id: '8_5',
+        name: 'AUG',
+        value: 0,
+      },
+      {
+        id: '9_5',
+        name: 'SEP',
+        value: 0,
+      },
+      {
+        id: '10_5',
+        name: 'OCT',
+        value: 0,
+      },
+      {
+        id: '11_5',
+        name: 'NOV',
+        value: 0,
+      },
+      {
+        id: '12_5',
+        name: 'DEC',
+        value: 0,
+      },
+    ],
+  },
+  {
+    store: {
+      id: 6,
+      name: 'Store 6',
+    },
+    months: [
+      {
+        id: '1_6',
+        name: 'JAN',
+        value: 0,
+      },
+      {
+        id: '2_6',
+        name: 'FEB',
+        value: 0,
+      },
+      {
+        id: '3_6',
+        name: 'MAR',
+        value: 0,
+      },
+      {
+        id: '4_6',
+        name: 'APR',
+        value: 0,
+      },
+      {
+        id: '5_6',
+        name: 'MAY',
+        value: 0,
+      },
+      {
+        id: '6_6',
+        name: 'JUN',
+        value: 0,
+      },
+      {
+        id: '7_6',
+        name: 'JUL',
+        value: 0,
+      },
+      {
+        id: '8_6',
+        name: 'AUG',
+        value: 0,
+      },
+      {
+        id: '9_6',
+        name: 'SEP',
+        value: 0,
+      },
+      {
+        id: '10_6',
+        name: 'OCT',
+        value: 0,
+      },
+      {
+        id: '11_6',
+        name: 'NOV',
+        value: 0,
+      },
+      {
+        id: '12_6',
+        name: 'DEC',
+        value: 0,
+      },
+    ],
+  },
+  {
+    store: {
+      id: 7,
+      name: 'Store 7',
+    },
+    months: [
+      {
+        id: '1_7',
+        name: 'JAN',
+        value: 0,
+      },
+      {
+        id: '2_7',
+        name: 'FEB',
+        value: 0,
+      },
+      {
+        id: '3_7',
+        name: 'MAR',
+        value: 0,
+      },
+      {
+        id: '4_7',
+        name: 'APR',
+        value: 0,
+      },
+      {
+        id: '5_7',
+        name: 'MAY',
+        value: 0,
+      },
+      {
+        id: '6_7',
+        name: 'JUN',
+        value: 0,
+      },
+      {
+        id: '7_7',
+        name: 'JUL',
+        value: 0,
+      },
+      {
+        id: '8_7',
+        name: 'AUG',
+        value: 0,
+      },
+      {
+        id: '9_7',
+        name: 'SEP',
+        value: 0,
+      },
+      {
+        id: '10_7',
+        name: 'OCT',
+        value: 0,
+      },
+      {
+        id: '11_7',
+        name: 'NOV',
+        value: 0,
+      },
+      {
+        id: '12_7',
+        name: 'DEC',
+        value: 0,
+      },
+    ],
+  },
+  {
+    store: {
+      id: 8,
+      name: 'Store 8',
+    },
+    months: [
+      {
+        id: '1_8',
+        name: 'JAN',
+        value: 0,
+      },
+      {
+        id: '2_8',
+        name: 'FEB',
+        value: 0,
+      },
+      {
+        id: '3_8',
+        name: 'MAR',
+        value: 0,
+      },
+      {
+        id: '4_8',
+        name: 'APR',
+        value: 0,
+      },
+      {
+        id: '5_8',
+        name: 'MAY',
+        value: 0,
+      },
+      {
+        id: '6_8',
+        name: 'JUN',
+        value: 0,
+      },
+      {
+        id: '7_8',
+        name: 'JUL',
+        value: 0,
+      },
+      {
+        id: '8_8',
+        name: 'AUG',
+        value: 0,
+      },
+      {
+        id: '9_8',
+        name: 'SEP',
+        value: 0,
+      },
+      {
+        id: '10_8',
+        name: 'OCT',
+        value: 0,
+      },
+      {
+        id: '11_8',
+        name: 'NOV',
+        value: 0,
+      },
+      {
+        id: '12_8',
+        name: 'DEC',
+        value: 0,
+      },
+    ],
+  },
+  {
+    store: {
+      id: 9,
+      name: 'Store 9',
+    },
+    months: [
+      {
+        id: '1_9',
+        name: 'JAN',
+        value: 0,
+      },
+      {
+        id: '2_9',
+        name: 'FEB',
+        value: 0,
+      },
+      {
+        id: '3_9',
+        name: 'MAR',
+        value: 0,
+      },
+      {
+        id: '4_9',
+        name: 'APR',
+        value: 0,
+      },
+      {
+        id: '5_9',
+        name: 'MAY',
+        value: 0,
+      },
+      {
+        id: '6_9',
+        name: 'JUN',
+        value: 0,
+      },
+      {
+        id: '7_9',
+        name: 'JUL',
+        value: 0,
+      },
+      {
+        id: '8_9',
+        name: 'AUG',
+        value: 0,
+      },
+      {
+        id: '9_9',
+        name: 'SEP',
+        value: 0,
+      },
+      {
+        id: '10_9',
+        name: 'OCT',
+        value: 0,
+      },
+      {
+        id: '11_9',
+        name: 'NOV',
+        value: 0,
+      },
+      {
+        id: '12_9',
+        name: 'DEC',
+        value: 0,
+      },
+    ],
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Table data={data} />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Table/index.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import './styles.css'
+
+const months = [
+  { id: 1, label: 'Магазины' },
+  { id: 2, label: 'Янв' },
+  { id: 3, label: 'Фев' },
+  { id: 4, label: 'Март' },
+  { id: 5, label: 'Апр' },
+  { id: 6, label: 'Май' },
+  { id: 7, label: 'Июнь' },
+  { id: 8, label: 'Июнь' },
+  { id: 9, label: 'Авг' },
+  { id: 10, label: 'Сен' },
+  { id: 14, label: 'Окт' },
+  { id: 11, label: 'Нояб' },
+  { id: 12, label: 'Дек' },
+  { id: 13, label: 'Итого по магазину' },
+]
+
+const Table = ({ data }) => {
+  const [budgets, setBudgets] = useState(data)
+
+  const handleBudgetChange = (storeIndex, monthIndex, value) => {
+    const newBudgets = [...budgets]
+    newBudgets[storeIndex].months[monthIndex].value = value
+    setBudgets(newBudgets)
+  }
+
+  const getTotalByStore = (storeIndex) => {
+    const storeBudgets = budgets[storeIndex].months
+    return storeBudgets.reduce((total, month) => total + month.value, 0)
+  }
+
+  const getTotalByMonth = (monthIndex) => {
+    return budgets.reduce(
+      (total, store) => total + store.months[monthIndex].value,
+      0
+    )
+  }
+
+  const calculateTotalOverall = () => {
+    return budgets.reduce((total, store) => {
+      const storeTotal = getTotalByStore(budgets.indexOf(store))
+      return total + storeTotal
+    }, 0)
+  }
+
+  return (
+    <div className="table">
+      <table className="budget-table">
+        <thead>
+          <tr>
+            {months.map((item) => (
+              <th key={item.id}>{item.label}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {budgets.map((store, storeIndex) => (
+            <tr key={store.store.id}>
+              <td className="title">{store.store.name}</td>
+              {store.months.map((month, monthIndex) => (
+                <td key={month.id}>
+                  <input
+                    className="table__input"
+                    type="number"
+                    value={month.value}
+                    onChange={(e) =>
+                      handleBudgetChange(
+                        storeIndex,
+                        monthIndex,
+                        parseInt(e.target.value)
+                      )}
+                  />
+                </td>
+              ))}
+              <td className="total">{getTotalByStore(storeIndex)}</td>
+            </tr>
+          ))}
+          <tr>
+            <td className="title">Итого по месяцу</td>
+            {budgets[0].months.map((month, monthIndex) => (
+              <td className="total" key={month.id}>
+                {getTotalByMonth(monthIndex)}
+              </td>
+            ))}
+            <td className="total">
+              <span>{calculateTotalOverall()}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
